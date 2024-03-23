c6748c20c5bd7519040b09f9df27f35ceb085703
tommysr <47206288+tommysr@users.noreply.github.com>
2024-03-23 14:44:17 +0100

    dummy routing

--- index.php
+++ index.php
@@ -5,4 +5,8 @@ require_once 'src/controllers/AppController.php';
 
 $controller = new AppController();
 
-$controller->render("dashboard");
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+$action = explode("/", $path)[0];
+$action = $action == null ? 'login': $action;
+$controller->render($action);
