68d03a73620439f3ba6f475556dbe63903c0ab63
tommysr <47206288+tommysr@users.noreply.github.com>
2024-04-16 13:49:24 +0200

    routing with quest controller

---
+++ src/controllers/QuestsController.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "AppController.php";
+require_once __DIR__ . '/../models/Quest.php';
+require_once __DIR__ . '/../repository/QuestRepository.php';
+
+
+class QuestsController extends AppController
+{
+  private $questRepository;
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->questRepository = new QuestRepository();
+  }
+
+  public function quests()
+  {
+      $quests = $this->questRepository->getQuests();
+      $this->render('quests', ['quests' => $quests]);
+  }
+}
