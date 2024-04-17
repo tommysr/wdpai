bce9fd12a9f1fd3e7e8d9f71357fe724c20813df
tommysr <47206288+tommysr@users.noreply.github.com>
2024-04-17 15:41:47 +0200

    registration controller

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -1,28 +1,76 @@
 <?php
 
 require_once "AppController.php";
+require_once __DIR__ . '/../models/User.php';
+require_once __DIR__ . '/../repository/UserRepository.php';
+require_once __DIR__ . '/../validators/Validator.php';
+
 
 class AuthController extends AppController
 {
+  private UserRepository $userRepository;
+
+
   public function __construct()
   {
     parent::__construct();
+    $this->userRepository = new UserRepository();
   }
 
-  public function login() {
-    if (!$this -> isPost()) {
-      return $this-> render('login', ['title' => 'Sign in']);
+  public function login()
+  {
+    if (!$this->isPost()) {
+      return $this->render('login', ['title' => 'Sign in']);
     }
 
     // TODO: Handle sign in
   }
 
-  public function register() {
-    if (!$this -> isPost()) {
-      return $this->render('register', ['title' => 'Sign up']);
+  public function register()
+  {
+    if (!$this->isPost()) {
+      return $this->renderRegisterView();
     }
 
-    // TODO: Handle register
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $confirmedPassword = $_POST['confirmedPassword'];
+    $repository = $this->userRepository;
+
+    $validationRules = [
+      ['validator' => 'validateEmail', 'value' => $email, 'errorMessage' => 'Invalid email'],
+      ['validator' => 'validatePassword', 'value' => $password, 'errorMessage' => 'Use a strong password (at least 8 characters)'],
+      ['validator' => 'validateUsername', 'value' => $username, 'errorMessage' => 'Invalid username'],
+      ['validator' => 'validateConfirmedPassword', 'value' => ['password' => $password, 'confirmedPassword' => $confirmedPassword], 'errorMessage' => 'Passwords do not match']
+    ];
+
+    foreach ($validationRules as $rule) {
+      $validator = $rule['validator'];
+      $value = $rule['value'];
+
+      if (!Validator::$validator($value)) {
+        return $this->renderRegisterView($rule['errorMessage']);
+      }
+    }
+
+    if ($repository->userExists($email)) {
+      return $this->renderRegisterView('This email is already registered');
+    }
+
+    if ($repository->userNameExists($username)) {
+      return $this->renderRegisterView('Username is already taken');
+    }
+
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+    $user = new User($email, $password_hash, $username, null);
+    $this->userRepository->addUser($user);
+
+    return $this->render('login', ['title' => 'Sign in', 'message' => 'registered']);
   }
 
+  private function renderRegisterView(string $message = '')
+  {
+    return $this->render('register', ['title' => 'Sign up', 'message' => $message]);
+  }
 }
