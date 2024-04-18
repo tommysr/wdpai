7c5e0ba3cf0a80fbe5d0742285b02e19b2c8233c
tommysr <47206288+tommysr@users.noreply.github.com>
2024-04-18 14:32:19 +0200

    list quest info in card

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -1,7 +1,9 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("mySidenav").style.opacity = "1";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.opacity = "0";
 }
