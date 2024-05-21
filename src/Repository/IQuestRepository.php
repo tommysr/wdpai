465d43a4f9ab8a9a5e01beb22eedc4574cca3d51
tommysr <47206288+tommysr@users.noreply.github.com>
2024-05-21 22:07:12 +0200

    repositories implement interfaces

---
+++ src/Repository/IQuestRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repository;
+use App\Models\IQuest;
+
+interface IQuestRepository
+{
+  public function saveQuest(IQuest $quest): int;
+  public function getQuestById($questId): ?IQuest;
+  public function getQuests(): array;
+  public function getCreatorQuests(int $creator): array;
+  public function getApprovedQuests(): array;
+  public function getQuestToApprove(): array;
+  public function approve(int $questId);
+  public function updateQuest(IQuest $quest);
+
+}
